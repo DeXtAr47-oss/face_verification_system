afbdb222d4d672e70f9d8fb5da0cadb4e5acc125
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2025-08-26 22:08:51 +0530

    updated the init file

--- src/__init__.py
+++ src/__init__.py
@@ -6,5 +6,6 @@ __version__ = "1.0.0"
 
 from .models import FaceEmbeddingNet, SiameseNet
 from .data import TripletDataset, FaceDataset
+from .face_verification import FaceVerification
 
-__all__ = ["FaceEmbeddingNet", "SiameseNet", "TripletDataset", "FaceDataset"]
+__all__ = ["FaceEmbeddingNet", "SiameseNet", "TripletDataset", "FaceDataset", "FaceVerification"]
